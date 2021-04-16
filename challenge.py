0b10d5ee4c4e004b6274b23e7662f56b806026e9
Kevin Chan <kevin@chanster.net>
2021-04-16 10:35:12 -0500

    fix typos

--- challenge.py
+++ challenge.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from jpapi import Client
+from datetime import datetime
 import json
-import datetime
+
 
 challenge = Client()
 
@@ -12,16 +13,16 @@ try:
     json_response = response.json()
     print(json_response['title'])
 except Exception as err:
-    print(f"Error occured: {err}")
+    print(f"Error occurred: {err}")
 
-# 2 Inject a field call time into the results for post number 100 and print while JSON record
+# 2 Inject a field called 'time' into the results for post number 100 and print while JSON record
 try:
     response = challenge.posts.get(100)
     json_response = response.json()
-    json_response['time'] = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    json_response['time'] = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
     print(json_response)
 except Exception as err:
-    print(f"Error occured: {err}")
+    print(f"Error occurred: {err}")
 
 # 3 Create new posts entry with the follow values
 try:
@@ -32,16 +33,16 @@ try:
     )
     response.raise_for_status
 except Exception as err:
-    print(f"Error occured: {err}")
+    print(f"Error occurred: {err}")
 
 # 4 Determine if your post was successful, and if it was, create a tuple
 if response.status_code == 201:
     json_response = response.json()
     data = (json_response['id'], response.status_code, response.headers['X-Powered-By'])
 else:
-    print(f"Challange 3 failed with error {response.status_code}")
+    print(f"Challenge #3 failed with error {response.status_code}: {response.text}")
 
-# 5 print tuple from setp 4
+# 5 print tuple from step 4
 print(data)
 
 # 6 delete record from step 3
@@ -49,4 +50,4 @@ try:
     response = challenge.posts.delete(data[0])
     print(f"Status: {response.status_code}\nX-Content-Type-Options: {response.headers['X-COntent-Type-Options']}")
 except Exception as err:
-    print(f"Error occured: {err}")
+    print(f"Error occurred: {err}")
