db56b08b3261bcfabe121b5738ebeb84230055d5
Kevin Chan <kevin@chanster.net>
2021-04-16 10:34:55 -0500

    add rest of resources

--- jpapi/client.py
+++ jpapi/client.py
@@ -1,6 +1,9 @@
 from .post import Post
 from .comment import Comment
 from .album import Album
+from .photo import Photo
+from .todo import Todo
+from .user import User
 
 class Client:
 
@@ -9,6 +12,9 @@ class Client:
         self.posts = Post(f"{self.entrypoint}")
         self.comments = Comment(f"{self.entrypoint}")
         self.albums = Album(f"{self.entrypoint}")
+        self.photos = Photo(f"{self.entrypoint}")
+        self.todos = Todo(f"{self.entrypoint}")
+        self.users = User(f"{self.entrypoint}")
     
     if "__name__" == "__main__":
         print(f"Entrypoint: {entrypoint}")
