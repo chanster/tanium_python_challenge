66846f5f5ea70ce63eec674eae1252451a2782d6
Kevin Chan <kevin@chanster.net>
2021-04-16 14:33:44 -0500

    add try blocks

--- jpapi/tests/test_post.py
+++ jpapi/tests/test_post.py
@@ -6,49 +6,58 @@ import pytest
 class TestPost():
 
     def test_get_all_posts(self):
-        posts = Post("http://jsonplaceholder.typicode.com")
-        response = posts.get()
-        assert response.status_code == 200
+        try:
+            posts = Post("http://jsonplaceholder.typicode.com")
+            response = posts.get()
+        except err.HTTPError:
+            assert False
+        finally:
+            assert response.status_code == 200
 
     def test_get_single_post(self):
-        posts = Post("http://jsonplaceholder.typicode.com")
-        response = posts.get(5)
-        assert response.status_code == 200
-    
-    def test_get_invalid_post(self):
         try:
             posts = Post("http://jsonplaceholder.typicode.com")
-            response = posts.get(500)
+            response = posts.get(5)
         except err.HTTPError:
-            assert True
+            assert False
+        finally:
+            assert response.status_code == 200
 
-    def test_add_valid_post(self):
-        posts = Post("http://jsonplaceholder.typicode.com")
-        response = posts.add(1, 'This is a Test', 'A test to add an item to the posts resource')
-        assert response.status_code == 201
-    
-    def test_add_invalid_post(self):
+    def test_add_post(self):
         try:
             posts = Post("http://jsonplaceholder.typicode.com")
-            # add post with user that doesn't exist
-            response = posts.add(3000, 'Test should fail', 'body')
+            response = posts.add(1, 'This is a Test', 'A test to add an item to the posts resource')
         except err.HTTPError:
-            assert True
+            assert False
+        finally:
+            assert response.status_code == 201
     
     def test_replace_post(self):
-        posts = Post("http://jsonplaceholder.typicode.com")
-        response = posts.replace(1,2, 'Replacement Ttile', 'Replacement body message.')
-        assert response.status_code == 200
+        try:
+            posts = Post("http://jsonplaceholder.typicode.com")
+            response = posts.replace(1,2, 'Replacement Ttile', 'Replacement body message.')
+        except err.HTTPError:
+            assert False
+        finally:
+            assert response.status_code == 200
 
     def test_modify_post(self):
-        posts =  Post("http://jsonplaceholder.typicode.com")
-        response = posts.modify(1, user_id = 7)
-        assert response.status_code == 200
+        try:
+            posts =  Post("http://jsonplaceholder.typicode.com")
+            response = posts.modify(1, user_id = 7)
+        except err.HTTPError:
+            assert False
+        finally:
+            assert response.status_code == 200
 
     def test_delete_valid_post(self):
-        posts =  Post("http://jsonplaceholder.typicode.com")
-        response = posts.delete(1)
-        assert response.status_code == 200
+        try:
+            posts =  Post("http://jsonplaceholder.typicode.com")
+            response = posts.delete(1)
+        except err.HTTPError:
+            assert False
+        finally:
+            assert response.status_code == 200
 
     def test_delete_invalid_post(self):
         try:
