be39daa5903bee10c7949a64af2b09c15526442c
Kevin Chan <kevin@chanster.net>
2021-04-18 21:47:44 -0500

    mock requests

--- jpapi/tests/test_album.py
+++ jpapi/tests/test_album.py
@@ -1,74 +1,109 @@
 from requests import exceptions as err
 from ..album import Album
+import requests_mock
 import pytest
 
+
+mock_data = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "quidem molestiae enim"
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "sunt qui excepturi placeat culpa"
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "omnis laborum odio"
+    },
+    {
+        "userId": 1,
+        "id": 4,
+        "title": "non esse culpa molestiae omnis sed optio"
+    }
+]
 
 class TestAlbum():
 
     def test_get_all_albums(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.get()
-        except err.HTTPError:
-            assert False
-        finally:
+        with requests_mock.Mocker() as mock:
+            mock.get('https://jsonplaceholder.typicode.com/albums', json = mock_data, status_code = 200)
+
+            response = Album('https://jsonplaceholder.typicode.com').get()
+            assert response.json() == mock_data
             assert response.status_code == 200
 
+
     def test_get_single_album(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.get(5)
-        except err.HTTPError():
-            assert False
-        finally:
+        with requests_mock.Mocker() as mock:
+            mock.get('https://jsonplaceholder.typicode.com/albums/1', json = mock_data[1], status_code = 200)
+
+            response = Album('https://jsonplaceholder.typicode.com').get(1)
+            assert response.json() == mock_data[1]
             assert response.status_code == 200
     
     def test_get_invalid_album(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.get(500)
-        except err.HTTPError():
-            assert True
+        with requests_mock.Mocker() as mock:
+            mock.get('https://jsonplaceholder.typicode.com/albums/500', status_code = 404)
+
+            with pytest.raises(err.HTTPError):
+                response = Album('https://jsonplaceholder.typicode.com').get(500)
+                assert response.status_code == 404
 
     def test_add_album(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.add(1, 'Test album title')
-        except err.HTTPError:
-            assert False
-        finally:
+        with requests_mock.Mocker() as mock:
+            mock.post(
+                'https://jsonplaceholder.typicode.com/albums',
+                headers = {'Content-Type': 'application/json'},
+                status_code = 201
+            )
+
+            response = Album('https://jsonplaceholder.typicode.com').add(1, 'Mock Title')
             assert response.status_code == 201
     
     def test_replace_album(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.replace(1, 20, 'Reaplacement Title')
-        except err.HTTPError:
-            assert False
-        finally:
+        with requests_mock.Mocker() as mock:
+            mock.put(
+                'https://jsonplaceholder.typicode.com/albums/1',
+                headers = {'Content-Type': 'application/json'},
+                status_code = 200
+            )
+
+            response = Album('https://jsonplaceholder.typicode.com').replace(1, 2, 'Mock Title')
             assert response.status_code == 200
     
     def test_modify_album(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.modify(4, title = 'A Modified Title')
-        except err.HTTPError:
-            assert False
-        finally:
+        with requests_mock.Mocker() as mock:
+            mock.patch(
+                'https://jsonplaceholder.typicode.com/albums/1',
+                headers = {'Content-Type': 'application/json'},
+                status_code = 200
+            )
+
+            response = Album('https://jsonplaceholder.typicode.com').modify(1, title = 'Mock Title')
             assert response.status_code == 200
     
     def test_delete_valid_album(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.delete(2)
-        except err.HTTPError:
-            assert False
-        finally:
+        with requests_mock.Mocker() as mock:
+            mock.delete(
+                'https://jsonplaceholder.typicode.com/albums/1',
+                status_code = 200
+            )
+
+            response = Album('https://jsonplaceholder.typicode.com').delete(1)
             assert response.status_code == 200
 
     def test_delete_invalid_album(self):
-        try:
-            albums = Album("http://jsonplaceholder.typicode.com")
-            response = albums.delete(5000)
-        except err.HTTPError:
-            assert True
+        with requests_mock.Mocker() as mock:
+            mock.delete(
+                'https://jsonplaceholder.typicode.com/albums/500',
+                status_code = 404
+            )
+
+            with pytest.raises(err.HTTPError):
+                response = Album('https://jsonplaceholder.typicode.com').delete(500)
+                assert response.status_code == 404
